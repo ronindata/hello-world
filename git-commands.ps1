63dd64fb1367bcbe95695f863bdd4c3f779f7602
Ronin Data <ronidata@outlook.com>
2021-09-23 22:02:55 +0100

    git

---
+++ git-commands.ps1
@@ -0,0 +1,6 @@
+git config --global user.email "ronindata@outlook.com"
+git config --global user.name "ronindata"
+
+https://www.jcchouinard.com/create-your-first-github-project-in-vscode/
+
+:space_invader: 
