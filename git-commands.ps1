732527d8d60d46deb0d7ed0f32409baae371f4a5
ronindata <91224673+ronindata@users.noreply.github.com>
2021-09-23 22:04:53 +0100

    Delete git-commands.ps1

--- git-commands.ps1
+++
@@ -1,6 +0,0 @@
-git config --global user.email "ronindata@outlook.com"
-git config --global user.name "ronindata"
-
-https://www.jcchouinard.com/create-your-first-github-project-in-vscode/
-
-:space_invader: 
